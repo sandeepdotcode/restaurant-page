9c14417c97ce549a98c2c10d1ba6720edf4d04f7
Sandeep K K <sandeepdotcode@gmail.com>
2023-03-06 20:33:48 +0530

    Install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
