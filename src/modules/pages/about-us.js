635d7a0230664da129ede53bd7d908025c24636b
Sandeep K K <sandeepdotcode@gmail.com>
2023-03-13 23:45:41 +0530

    Create & Style Chef Div and contents

--- src/modules/pages/about-us.js
+++ src/modules/pages/about-us.js
@@ -2,6 +2,7 @@ import '../../styles/about-us.css';
 import addTextAnimation from '../functions/text-anim';
 import mrWorldwideImg from '../../assets/images/mrworldwide.png';
 import mrSushi from '../../assets/images/mr-sushi.png';
+import chefImage from '../../assets/images/pitbull-chef.png';
 
 function aboutUs() {
   const main = document.createElement('div');
@@ -29,7 +30,7 @@ function aboutUs() {
   managerName.className = 'manager-name';
 
   const managerBio = document.createElement('p');
-  managerBio.classList = 'manager-bio';
+  managerBio.className = 'manager-bio';
   managerBio.textContent = `The Man, The Myth, The LEGEND. The founder, manager, and executive chef of 
                             Worldwide Kitchen. Prior to starting the restaurant he had a successful career
                             as one of the world's most popular pop singers. It was always his dream to bring
@@ -47,7 +48,27 @@ function aboutUs() {
   mistersDish.src = mrSushi;
   managerDiv.append(managerImg, mistersDish);
 
-  main.appendChild(managerDiv);
+  const chefDiv = document.createElement('div');
+  chefDiv.className = 'chef-div';
+  const chefTitle = document.createElement('h5');
+  chefTitle.className = 'chef-title';
+  chefTitle.textContent = 'SOUS CHEF';
+  const chefName = document.createElement('h2');
+  chefName.className = 'chef-name';
+  chefName.textContent = 'Pitbull Woofmeister';
+
+  const chefBio = document.createElement('p');
+  chefBio.className = 'chef-bio';
+  chefBio.textContent = `Doggo ipsum puggorino woofer you are doin me a concern stop it fren shibe, long woofer length boy very taste wow. Wrinkler ruff you are doing me the shock yapper big ol, vvv ur givin me a spook super chub. Clouds super chub wrinkler heckin smol porgo, big ol long woofer much ruin diet blep. Fat boi he made many woofs corgo woofer pupper, boof wow very biscit doing me a frighten. Pats wow such tempt shibe doggo vvv waggy wags, aqua doggo heckin maximum borkdrive.
+  `;
+  chefDiv.append(chefTitle, chefName, chefBio);
+
+  const chefImg = document.createElement('img');
+  chefImg.className = 'chef-image';
+  chefImg.src = chefImage;
+  chefDiv.appendChild(chefImg);
+
+  main.append(managerDiv, chefDiv);
 
   return main;
 }
